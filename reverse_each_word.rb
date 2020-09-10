88ba5a7edd9c7b1217ceb94745bb5589e92b95ae
Dasha Lary <dk2800@nyu.edu>
2020-09-10 19:54:41 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,6 @@
 def reverse_each_word(sentence)
   newsentence = sentence.split(" ")
-  result = []
-  reverse_each_word(sentence).each do
+  reverse_each_word(newsentence).each do
   |word| puts "#{word}.reverse << result"
 end 
  return result
